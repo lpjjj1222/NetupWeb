8a693428dc7cec432826195d593c88cba574d847
lpjjj1222 <694676764@qq.com>
2024-06-27 18:19:16 -0700

    ..

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Netup",
   description: "We provide a premier platform for connecting with professionals. ",
-  url: "https://github.com/saasfly/saasfly",
+  url: "https://github.com/NetupApp",
   ogImage: "",
   links: {
     github: "https://github.com/NetupApp",
