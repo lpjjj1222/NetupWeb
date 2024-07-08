077c9baa5aab9ec273754e11123a968d83d0ae03
lpjjj1222 <694676764@qq.com>
2024-07-07 19:53:52 -0700

    delete auth related in middleware

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -7,19 +7,6 @@ import { withAuth } from "next-auth/middleware";
 
 import { i18n } from "~/config/i18n-config";
 
-const noNeedProcessRoute = [".*\\.png", ".*\\.jpg", ".*\\.opengraph-image.png"];
-
-const noRedirectRoute = ["/api(.*)", "/trpc(.*)", "/admin"];
-
-const publicRoute = [
-  "/(\\w{2}/)?signin(.*)",
-  "/(\\w{2}/)?terms(.*)",
-  "/(\\w{2}/)?privacy(.*)",
-  "/(\\w{2}/)?docs(.*)",
-  "/(\\w{2}/)?blog(.*)",
-  "/(\\w{2}/)?pricing(.*)",
-  "^/\\w{2}$", // root with locale
-];
 
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
@@ -35,20 +22,6 @@ function getLocale(request: NextRequest): string | undefined {
   return 'en';
 }
 
-function isNoRedirect(request: NextRequest): boolean {
-  const pathname = request.nextUrl.pathname;
-  return noRedirectRoute.some((route) => new RegExp(route).test(pathname));
-}
-
-function isPublicPage(request: NextRequest): boolean {
-  const pathname = request.nextUrl.pathname;
-  return publicRoute.some((route) => new RegExp(route).test(pathname));
-}
-
-function isNoNeedProcess(request: NextRequest): boolean {
-  const pathname = request.nextUrl.pathname;
-  return noNeedProcessRoute.some((route) => new RegExp(route).test(pathname));
-}
 
 /**
  * 1、 if the request is public page and don't have locale, redirect to locale page
@@ -58,13 +31,7 @@ function isNoNeedProcess(request: NextRequest): boolean {
  * @returns
  */
 export default async function middleware(request: NextRequest) {
-  if (isNoNeedProcess(request)) {
-    return null;
-  }
-  const isWebhooksRoute = /^\/api\/webhooks\//.test(request.nextUrl.pathname);
-  if (isWebhooksRoute) {
-    return NextResponse.next();
-  }
+
   const pathname = request.nextUrl.pathname;
 
   // 检查请求路径是否为 /download 或 /en/download
@@ -80,7 +47,7 @@ export default async function middleware(request: NextRequest) {
   );
   
   // Redirect if there is no locale
-  if (!isNoRedirect(request) && pathnameIsMissingLocale) {
+  if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
     return NextResponse.redirect(
       new URL(
@@ -90,57 +57,7 @@ export default async function middleware(request: NextRequest) {
     );
   }
 
-
-  if (isPublicPage(request)) {
-    return null;
-  }
-  // @ts-ignore
-  return authMiddleware(request, null);
 }
-
-const authMiddleware = withAuth(
-  async function middlewares(req) {
-    const token = await getToken({ req });
-    const isAuth = !!token;
-    const isAdmin = token?.isAdmin;
-    const isAuthPage = /^\/[a-zA-Z]{2,}\/(login|register)/.test(
-      req.nextUrl.pathname,
-    );
-    const isAuthRoute = /^\/api\/trpc\//.test(req.nextUrl.pathname);
-    const locale = getLocale(req);
-
-    if (isAuthRoute && isAuth) {
-      return NextResponse.next();
-    }
-    if (req.nextUrl.pathname.startsWith("/admin/dashboard")) {
-      if (!isAuth || !isAdmin)
-        return NextResponse.redirect(new URL(`/admin/login`, req.url));
-      return NextResponse.next();
-    }
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL(`/${locale}/dashboard`, req.url));
-      }
-      return null;
-    }
-    if (!isAuth) {
-      let from = req.nextUrl.pathname;
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search;
-      }
-      return NextResponse.redirect(
-        new URL(`/${locale}/login?from=${encodeURIComponent(from)}`, req.url),
-      );
-    }
-  },
-  {
-    callbacks: {
-      authorized() {
-        return true;
-      },
-    },
-  },
-);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
