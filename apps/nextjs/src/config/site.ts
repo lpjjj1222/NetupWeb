b822ea0e567899ac6430334c14850c0ca3820b72
Joey <joeyzhu2022@gmail.com>
2024-07-10 01:23:55 -0700

    add: link

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Netup Social",
   description: "Netup is a career networking app that helps users build career connections in a more sincere, fun, and personalized way.",
-  url: "https://netup.social",
+  url: "https://www.netup.social",
   ogImage: "https://www.netup.social/newBigLogo.png",
   links: {
     github: "https://www.netup.social",
