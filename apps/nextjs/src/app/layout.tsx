8e01ea9ed7ae6c5376e14f39f564f6a11fd32e71
Joey <joeyzhu2022@gmail.com>
2024-07-10 00:45:06 -0700

    add: icon.png

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -62,7 +62,7 @@ export const metadata = {
     description: siteConfig.description,
     siteName: siteConfig.name,
     images: [{
-      url: "https://www.netup.social/newBigLogo.png",
+      url: "https://www.netup.social/icon.png",
       width: 120,
       height: 120,
       alt: 'Netup App Icon',
