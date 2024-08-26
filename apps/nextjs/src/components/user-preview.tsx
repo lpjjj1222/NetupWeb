ecd774a827858e95b424de22b1567e933b6c392b
jamespanyp64 <87825062+jamespanyp64@users.noreply.github.com>
2024-08-26 00:49:13 -0700

    Update user-preview.tsx

--- apps/nextjs/src/components/user-preview.tsx
+++ apps/nextjs/src/components/user-preview.tsx
@@ -207,14 +207,14 @@ const UserPreview = ({ userId }: any) => {
         padding: "8px 0",
         backgroundColor: Colors.appColorSecondary,
         color: Colors.white,
-        fontSize: "1.2rem",
-        fontWeight: "500",
+        fontSize: "0.9rem",
+        fontWeight: "400",
         position: "fixed",
         bottom: 45,
         left: '50%',
         transform: 'translateX(-50%)',
         borderRadius: 20,
-        width: "48%",
+        width: "42%",
       }}>
         <button 
         style={{
