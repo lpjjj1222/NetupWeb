cf466b815ace65311961b2104c6c55388d968e2e
Joey <joeyzhu2022@gmail.com>
2024-08-25 19:37:37 -0700

    add back: [lang]

--- apps/nextjs/src/config/i18n-config.ts
+++ apps/nextjs/src/config/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-  defaultLocale: "zh",
-  locales: ["en", "zh", "ko", "ja"],
+  defaultLocale: "en",
+  locales: ["en"],
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
@@ -8,7 +8,4 @@ export type Locale = (typeof i18n)["locales"][number];
 // 新增的映射对象
 export const localeMap = {
   en: "English",
-  zh: "中文",
-  ko: "한국어",
-  ja: "日本語",
 } as const;
