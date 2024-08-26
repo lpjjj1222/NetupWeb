35ae2d5b8b9187118e19777dc457637440ff6199
Joey <joeyzhu2022@gmail.com>
2024-08-25 19:02:54 -0700

    add: directly return json in router apple-app-site-association

--- apps/nextjs/src/app/apple-app-site-association/page.tsx
+++ apps/nextjs/src/app/apple-app-site-association/page.tsx
@@ -3,8 +3,26 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function TermsOfUse() {
+export default function AppleAppSiteAssociation() {
     const router = useRouter();
+
+    return (
+        {
+            "applinks": {
+                "apps": [],
+                "details": [
+                    {
+                        "appID": "HBSAFB9N3N.social.netup.netup",
+                        "paths": [ "*"]
+                    },
+                    {
+                     "appID": "HBSAFB9N3N.social.netup",
+                     "paths": [ "*"]
+                     }
+                ]
+            }
+         }
+    )
 
     useEffect(() => {
         router.push("/public/apple-app-site-association");
